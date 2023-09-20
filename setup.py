047c20f9e72e42e61e2ff696e1dffd70e16fa011
GeorgeSousa <george.sousa@unisoma.com>
2023-09-20 15:20:14 -0300

    fix change version for update in path

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ def read(fname):
 setup(
     name='databoxlib',
     packages=find_packages(include=['databoxlib']),
-    version='0.1.0',
+    version='0.1.1',
     description='Python library to interact with databox',
     author='DataEngineeringUnisoma',
     license='MIT',
